b977f4fcc8ddc485a954f4f8fdb5ee187fc84e7a
leviwp48 <leviwp48@gmail.com>
2023-12-14 16:00:53 -0800

    Finished day 2

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -1,67 +1,89 @@
+from typing import Dict, List
 import re
 
-CONSTRAINTS = {
-    'red': 12,
-    'green': 13,
-    'blue': 14
-}
-
-FILENAME = 'test.txt'
-
-def parse_games() -> {}:
-    game_dict = {}
-    with open(FILENAME, 'r') as f:
-        for content in f:
-            match = re.match(r'Game (\d+): (.+)', content)
-            game_number, game_content = match.groups()
-            game_dict[game_number] = []
-            parts = game_content.split(';')
-            for part in parts:
-                part = part.strip()
-                color_counts = {}
-                color_matches = re.findall(r'(\d+) (\w+)', part)
-                for count, color in color_matches:
-                    color_counts[color] = int(count)
-                game_dict[game_number].append(color_counts)
-            
-    return game_dict
-                
-def check_and_add_games(game_dict: dict) -> int: 
-    game_sum = 0
-    skip_game = False
-    for game in game_dict:
-        print('on this game: ', game)
-        if skip_game:
-            print('setting skip back to False on this game: ', game)
-            skip_game = False
-            pass
-        else:
-            for cubes in game_dict[game]:
-                print('on this set of colors: ', cubes)
-                # print(cubes)
-                if skip_game:
-                    print('breaking out of this set of colors since skip is True: ', color, ' still on this game: ', game)
-                    # print('skipping')
-                    break
-                for color in CONSTRAINTS:
-                    print('on this one color: ', color)
-                    # print(color)
-                    if cubes.get(color):
-                        if cubes[color] > CONSTRAINTS[color]:
-                            # print('too big')
-                            print('this color is not valid: ', cubes[color])
-                            skip_game = True
-                            break
-
-        if not skip_game:
-            print('adding game: ', game)
-            game_sum += int(game)
-            print(' game sum is now: ', game_sum)
-
-    return game_sum
+class GameChecker:
+    DEFAULT_FILENAME = 'test.txt'
+    DEFAULT_CONSTRAINTS = {
+        'red': 12,
+        'green': 13,
+        'blue': 14
+    }
 
 
+    def __init__(self, filename: str = DEFAULT_FILENAME, constraints: Dict[str, int] = None):
+        self.filename = filename
+        self.constraints = constraints or self.DEFAULT_CONSTRAINTS
+        self.game_dict = None  
+        self.power_sum = 0 # sum of the powers of the min colors of each valid game
 
-res = parse_games()
-sum = check_and_add_games(res)
-print(sum)
+
+    def parse_games(self) -> Dict[str, List[Dict[str, int]]]:
+        """
+        Parse the games from the specified file and set the instance variable game_dict.
+        """
+        self.game_dict = {}
+        with open(self.filename, 'r') as f:
+            for content in f:
+                match = re.match(r'Game (\d+): (.+)', content)
+                if match:
+                    game_number, game_content = match.groups()
+                    self.game_dict[game_number] = []
+                    parts = game_content.split(';')
+                    for part in parts:
+                        part = part.strip()
+                        color_counts = {}
+                        color_matches = re.findall(r'(\d+) (\w+)', part)
+                        for count, color in color_matches:
+                            color_counts[color] = int(count)
+                        self.game_dict[game_number].append(color_counts)
+        return self.game_dict
+
+
+    def check_and_add_games(self) -> int:
+        """
+        Check games against constraints and return the sum of valid games.
+        """
+        game_sum = 0
+        for game in self.game_dict:
+            if not self.should_skip_game(self.game_dict[game]):
+                game_sum += int(game)
+        return game_sum
+
+
+    def should_skip_game(self, cubes_list: List[Dict[str, int]]) -> bool:
+        """
+        Check if a game should be skipped based on constraints.
+        """
+        for cubes in cubes_list:
+            for color in self.constraints:
+                if cubes.get(color, 0) > self.constraints[color]:
+                    return True
+        return False
+
+
+    def minimum_cubes(self):
+        """
+        Find minimum number of cube color for every game and sum the multiplied value of those minimums
+        """
+        for game in self.game_dict:
+            min_colors = {'red': 0, 'green': 0, 'blue': 0}
+            for cubes in self.game_dict[game]:
+                for color in cubes:
+                    min_colors[color] = max(int(cubes[color]), min_colors[color])
+            self.power_sum += min_colors['red'] * min_colors['green'] * min_colors['blue']
+        return self.power_sum
+        
+
+
+if __name__ == "__main__":
+    game_checker = GameChecker()
+
+    # Part 1
+    games = game_checker.parse_games()
+    result = game_checker.check_and_add_games()
+    print(result)
+
+    # Part 2
+    game_checker.minimum_cubes()
+    print(game_checker.power_sum)
+
